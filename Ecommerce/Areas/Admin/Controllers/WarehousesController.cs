67469dda342d359b7740a90a905909ec4091f9f2
Sebastian <Sebastianbrand046@outlook.com>
2023-10-02 16:21:34 -0500

    Starting winery view with call to controller

---
+++ Ecommerce/Areas/Admin/Controllers/WarehousesController.cs
@@ -0,0 +1,59 @@
+﻿using Ecommerce.AccessData.Repository.IRepository;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WarehousesController : Controller
+    {
+        private readonly IWorkContainer _workContainer;
+
+        public WarehousesController(IWorkContainer workContainer)
+        {
+            _workContainer = workContainer;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Warehouse warehouse = new Warehouse();
+            if (id == null)
+            {
+                //Create New Warehouse
+                warehouse.State = true;
+                return View(warehouse);
+            }
+
+            //Update Warehouse
+
+            warehouse = await _workContainer.warehouse.Get(id.GetValueOrDefault());
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(warehouse); 
+        }
+
+
+
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var all = await _workContainer.warehouse.GetAll();
+            return Json(new { data = all }); 
+        }
+
+        #endregion
+    }
+}
---
+++ Ecommerce/wwwroot/js/Warehouse.js
@@ -0,0 +1,52 @@
+﻿let databable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    databable = $('#tblData').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ Registros Por Pagina",
+            "zeroRecords": "Ningun Registro",
+            "info": "Mostrar page _PAGE_ de _PAGES_",
+            "infoEmpty": "no hay registros",
+            "infoFiltered": "(filtered from _MAX_ total registros)",
+            "search": "Buscar",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "ajax": {
+            "url": "/Admin/Warehouses/GetAll"
+        },
+        "columns": [
+            { "data": "name" , "width": "20%"},
+            { "data": "description", "width": "40%" },
+            {
+                "data": "state",
+                "render": function (data) {
+                    if (data == true) {
+                        return "Activo";
+                    } else {
+                        return "Inactivo";
+                    }
+                }, "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                    <a href="/Admin/Warehouses/Upsert/${data}" class="btn btn-success text-white" style="curos:pointer">
+                    <i class="bi bi-pencil-square"></i> Edtar</a>
+                    <a onclick=Delete("/Admin/Warehouses/Delete/${data}") class="btn btn-danger text-white" style="curos:pointer">
+                    <i class="bi bi-trash3-fill"></i> Eliminar</a>
+                    </div>`;
+                }, "width": "20%"
+            }
+        ]
+    });
+}
